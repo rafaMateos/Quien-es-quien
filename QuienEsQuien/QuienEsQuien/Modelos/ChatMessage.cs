e5bf001e7caf739c9a3ac0a9e9b8f166c606dade
Dylan <dylanadriansoler@gmail.com>
2019-01-21 13:59:35 +0100

    weeeee

---
+++ QuienEsQuien/QuienEsQuien/Modelos/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuienEsQuien.Modelos
+{
+    public class ChatMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+
+    }
+}
---
+++ QuienEsQuien/QuienEsQuien/Modelos/clsCarta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuienEsQuien.Modelos
+{
+    public class clsCarta
+    {
+        #region Propiedades privadas
+        private String _imagenUri;
+        private bool _estaBajada;
+        private bool _esGanadora;
+        #endregion
+
+        #region Propiedades publicas
+        public String imagenUri
+        {
+            get
+            {
+                return _imagenUri;
+            }
+            set
+            {
+               _imagenUri = value;
+            }
+        }
+
+        public bool estaBajada
+        {
+            get
+            {
+                return _estaBajada;
+            }
+            set
+            {
+                _estaBajada = value;
+            }
+        }
+
+        public bool esGanadora
+        {
+            get
+            {
+                return _esGanadora;
+            }
+            set
+            {
+                _esGanadora = value;
+            }
+        }
+        #endregion Propiedades publicas
+    }
+}
