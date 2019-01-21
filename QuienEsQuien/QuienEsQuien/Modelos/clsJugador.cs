d83be8cccf4cf4ef31c37b7d16d9afc47b6cba2e
RafaMateos <rafaelmateostorres@gmail.com>
2019-01-21 13:32:52 +0100

    Dylan descaga

---
+++ QuienEsQuien/QuienEsQuien/Modelos/clsJugador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuienEsQuien.Modelos
+{
+    public class clsJugador
+    {
+        #region Propiedades privadas
+        private String _nickname;
+        private int _id;
+        private clsRespuesta _respuestas;
+        private bool esGanador;
+        #endregion
+
+        #region Propiedades publicas
+        public String NickName {
+
+            get {
+
+                return _nickname;
+            }
+            set {
+
+                _nickname = value;
+            }
+
+        }
+        public int Id {
+
+            get {
+
+                return _id;
+            }
+            set {
+
+                _id = value;
+            }
+        }
+        public clsRespuesta Respuestas {
+
+            get {
+
+                return _respuestas;
+
+            }
+            set {
+
+                _respuestas = value;
+            }
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ QuienEsQuien/QuienEsQuien/Modelos/clsRespuesta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuienEsQuien.Modelos
+{
+    public class clsRespuesta
+    {
+
+        #region Propiedades privadas
+        private int _intentos;
+        private int _cartasBajadas;
+        #endregion
+
+        #region Propiedades Publicas
+        public int Intentos{
+            get {
+
+                return _intentos;
+            }
+            set {
+
+                _intentos = value;
+            }
+        }
+        public int CartasBajadas {
+
+            get {
+
+                return _cartasBajadas;
+
+            }
+            set {
+
+                _cartasBajadas = value;
+            }
+        }
+        #endregion
+
+
+
+    }
+}
