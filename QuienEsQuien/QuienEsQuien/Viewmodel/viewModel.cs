5c983272556d762424b3fa5f79348e6cff453ace
lz <luiszumarraga95@gmail.com>
2019-02-07 14:40:34 +0100

    Viewmodel y vista

--- QuienEsQuien/QuienEsQuien/Viewmodel/viewModel.cs
+++ QuienEsQuien/QuienEsQuien/Viewmodel/viewModel.cs
@@ -8,7 +8,25 @@ using System.Threading.Tasks;
 namespace QuienEsQuien.Viewmodel {
     public class viewModel : clsBase {
 
+        #region propiedades privadas
         private List<clsSala> _ListadoDeSalas;
+
+        private clsSala _salaSeleccionada;
+
+        #endregion
+
+        #region propiedades publicas
+        public List<clsSala> listadoDeSalas {
+            get { return _ListadoDeSalas; }
+            set { _ListadoDeSalas = value; }
+        }
+
+        public clsSala salaSeleccionada {
+            get { return _salaSeleccionada; }
+            set { _salaSeleccionada = value; }
+        }
+
+        #endregion
 
     }
 }
