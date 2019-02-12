2ca4ee62a640e7d4170deca412affafe029a97b4
RafaMateos <rafaelmateostorres@gmail.com>
2019-02-12 14:36:39 +0100

    gfgfg

--- QuienEsQuien/QuienEsQuien/Views/loby_screen.xaml.cs
+++ QuienEsQuien/QuienEsQuien/Views/loby_screen.xaml.cs
@@ -55,16 +55,25 @@ namespace QuienEsQuien.Views {
             conn.Start();
 
             SalasProxy.On<clsSala>("ContarUsuarios", onInfo);
-            SalasProxy.On<String>("PasarAJuego", PasarAJugar);
+             SalasProxy.On<String>("PasarAJuego", PasarAJugar);
             //ChatProxy.On<ChatMessage>("agregarMensaje", addMessage);
          
         }
 
-        private void PasarAJugar(string salaNombre) {
+        
+        private async void PasarAJugar(string salaNombre) {
 
-            this.Frame.Navigate(typeof(game_screen), salaNombre);
+
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () => {
+
+                this.Frame.Navigate(typeof(game_screen), salaNombre);
+
+            });
+
+            
 
         }
+        
 
         private async void addMessage(ChatMessage obj)
         {
@@ -112,7 +121,14 @@ namespace QuienEsQuien.Views {
                
             });
 
-          
+
+            if (conn.State == Microsoft.AspNet.SignalR.Client.ConnectionState.Connected) {
+
+                SalasProxy.Invoke("PasarAsalaServer", obj.nombre);
+
+            }
+
+
         }
 
     }
